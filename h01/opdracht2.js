b2614b432ce2c64127147b8b1beee0129949fa54
Smisidjan <Sarai.misidjan@hotmail.com>
2020-01-15 13:51:26 +0100

    Hoofdstuk 1

---
+++ h01/opdracht2.js
@@ -0,0 +1,3 @@
+for(var i = 0; i < 10; i++) {
+    console.log("De waarde van i is " + i)
+}
---
+++ h01/opdracht3.js
@@ -0,0 +1,9 @@
+var mijnauto = {
+    merk: "Ford",
+    type: "Mondeo",
+    aantalWielen: 4,
+    kleur: "blauw"
+}
+
+console.log(mijnauto.kleur);
+
