4457d5fa93c13eeaf530c18b2369f16c9e9064b7
Smisidjan <Sarai.misidjan@hotmail.com>
2020-01-16 13:42:04 +0100

    Hoofdstuk 3

---
+++ h02/opdracht1.js
@@ -0,0 +1,4 @@
+document.getElementById("paginatitel").innerHTML = "Dit is de paginatitel";
+document.getElementById("subkop").innerHTML = "Dit is de subkop";
+document.getElementById("aap1").src = "../images/aap1.jpg";
+document.getElementById("aap2").src = "../images/aap2.jpg";
---
+++ h02/opdracht2.js
@@ -0,0 +1,4 @@
+document.getElementById("bl1").style.backgroundColor = "blue";
+document.getElementById("bl2").style.backgroundColor = "green";
+document.getElementById("bl3").style.backgroundColor = "yellow";
+document.getElementById("bl4").style.backgroundColor = "red";
