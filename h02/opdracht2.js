aae311a8865a92e95e5aac3f8a519509028e1a71
Smisidjan <Sarai.misidjan@hotmail.com>
2020-01-21 19:50:57 +0100

    Hoofdstuk 2

--- h02/opdracht2.js
+++ h02/opdracht2.js
@@ -1,4 +1,18 @@
 document.getElementById("bl1").style.backgroundColor = "blue";
+document.getElementById("bl1").style.backgroundColor = "blue";
+document.getElementById("bl1").style.textAlign = "center";
+
+
+
 document.getElementById("bl2").style.backgroundColor = "green";
+document.getElementById("bl2").style.textAlign = "center";
+document.getElementById("bl2").style.marginTop = "15px";
+
 document.getElementById("bl3").style.backgroundColor = "yellow";
+document.getElementById("bl3").style.marginTop = "40px";
+document.getElementById("bl3").style.textAlign = "right";
+
+
 document.getElementById("bl4").style.backgroundColor = "red";
+document.getElementById("bl4").style.textJustify = "auto";
+
